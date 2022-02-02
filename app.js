c51ffabdd737317d5046de06d9c3da1ff10e8515
genelem <genesiselealmunoz@gmail.com>
2022-02-02 19:55:12 -0300

    cambios

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+//app.use(express.static('public'));
+const path = require('path')
+app.use(express.static(path.resolve(__dirname, './public')))
 
-
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3015, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
